986c6be28a1e46d388987d976c0754130a85e923
Abhinav Sharma <me@abhinavsharma.me>
2011-06-13 13:48:39 -0700

    first shot code actually thumbnails, maybe I'm getting good at this.

--- bootstrap.js
+++ bootstrap.js
@@ -1,180 +1,51 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Restartless.
- *
- * The Initial Developer of the Original Code is The Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *    Abhinav Sharma <asharma@mozilla.com>
- *    Edward Lee <edilee@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-/* alias for quick access */
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
-
-/* imports */
+const Cu = Components.utils;
 Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/PlacesUtils.jsm");
-Cu.import("resource://gre/modules/AddonManager.jsm");
-Cu.import("resource://services-sync/util.js");
 
-/* Javascript files to import from scripts/ */
-AWESOMETAB_SCRIPTS = [
-  "awesometab",
-  "utils",
-  "collector",
-  "ranker",
-  "grandcentral",
-  "allsearch",
-  "stop",
-  "bmsearch",
-  "pos",
-  "mixer",
-  "display",
-  "tester",
-];
 
-const global = this;
-const DEBUG = true;
-const TESTER = true;
-const reportError = DEBUG ? Cu.reportError : function() {};
-const J = DEBUG ? JSON.stringify : function() {return ""};
-
-/* some useful regular expressions */
-RE_NOUN_VERB = new RegExp(/(^NN)|(^VB)|(^JJ)/);
-
-/*http{s}://<anything goes here>{/} types of URLs are good */
-RE_HOME_URL = new RegExp(/^https{0,1}:\/\/[a-zA-Z0-9\.\-\_]+\/{0,1}$/);
-
-/*
- * 1. has an unacceptable substring like /post/ or /article/
- * 2. ends with a number like bla.com/2/ or bla.com/2
- * 3. has 8 or more consecutive numbers, ignoring slashes
- */
-RE_FAIL_URL = new RegExp(/(\/post\/|\/article\/)|([\/#][0-9]+\/{0,1}$)|((\/*[0-9]){8,})/)
-
-/**
- * Synchronously query with an async statement fetching results by name
- */
-function spinQuery(connection, {names, params, query}) {
-  // Initialize the observer to watch for application quits during a query
-  if (spinQuery.checkReady == null) {
-    // In the common case, return true to continue execution
-    spinQuery.checkReady = function() true;
-
-    // Change the checkReady function to throw to abort
-    let abort = function(reason) {
-      spinQuery.checkReady = function() {
-        throw reason;
-      };
-    };
-
-    // Add the observer and make sure to clean up
-    let onQuit = function() abort("Application Quitting");
-    Services.obs.addObserver(onQuit, "quit-application", false);
-    unload(function() Services.obs.removeObserver(onQuit, "quit-application"));
-
-    // Also watch for unloads to stop queries
-    unload(function() abort("Add-on Unloading"));
+function getThumbnail(win, doc) {
+  let canvas = doc.createElement("canvas"); // where?
+  canvas.setAttribute('width', '90');
+  canvas.setAttribute('height', '70');
+  let aspectRatio = canvas.width / canvas.height;
+  let w = win.innerWidth + win.scrollMaxX;
+  let h = Math.max(win.innerHeight, w / aspectRatio);
+  if (w > 10000) {
+    w = 10000;
+  }
+  if (h > 10000) {
+    h = 10000;
   }
 
-  // Remember the results from processing the query
-  let allResults = [];
-  let status;
+    let canvasW = canvas.width;
+    let canvasH = canvas.height;
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvasW, canvasH);
+    ctx.save();
 
-  // Nothing to do with no query
-  if (query == null)
-    return allResults;
-
-  // Create the statement and add parameters if necessary
-  let statement = connection.createAsyncStatement(query);
-  if (params != null) {
-    Object.keys(params).forEach(function(key) {
-      statement.params[key] = params[key];
-    });
-  }
-
-  // Start the query and prepare to cancel if necessary
-  let pending = statement.executeAsync({
-    // Remember that we finished successfully
-    handleCompletion: function handleCompletion(reason) {
-      if (reason != Ci.mozIStorageStatementCallback.REASON_ERROR)
-        status = allResults;
-    },
-
-    // Remember that we finished with an error
-    handleError: function handleError(error) {
-      status = error;
-    },
-
-    // Process the batch of results and save them for later
-    handleResult: function handleResult(results) {
-      let row;
-      while ((row = results.getNextRow()) != null) {
-        let item = {};
-        names.forEach(function(name) {
-          item[name] = row.getResultByName(name);
-        });
-        allResults.push(item);
-      }
-    },
-  });
-
-  // Grab the current thread so we can make it give up priority
-  let thread = Cc["@mozilla.org/thread-manager;1"].getService().currentThread;
-
-  // Keep waiting until the query finished unless aborting
-  try {
-    while (spinQuery.checkReady() && status == null)
-      thread.processNextEvent(true);
-  }
-  // Must be aborting, so cancel the query
-  catch(ex) {
-    pending.cancel();
-    status = ex;
-  }
-
-  // Must have completed with an error so expose it
-  if (status != allResults)
-    throw status;
-
-  return allResults;
+    let scale = canvasH / h;
+    ctx.scale(scale, scale);
+  ctx.drawWindow(win, 0, 0, w, h, "rgb(255,255,255)");
+  ctx.restore();
+  let img = canvas.toDataURL("image/png", "");
+  return img;
 }
 
-/**
- * Apply a callback to each open and new browser windows.
- *
- * @usage watchWindows(callback): Apply a callback to each browser window.
- * @param [function] callback: 1-parameter function that gets a browser window.
- */
-function watchWindows(callback) {
-  // Wrap the callback in a function that ignores failures
+function setupListener(window) {
+  let tabs = window.gBrowser.visibleTabs;
+  for (let i = 0; i <  tabs.length; i++) {
+    let tab = tabs[i];
+    let doc = tab.linkedBrowser.contentDocument;
+    let win = tab.linkedBrowser.contentWindow;
+    let img = getThumbnail(win, doc);
+    Cu.reportError(img);
+  }
+}
+
+
+function startup(data, reason) {
+  
+  let callback = setupListener;
+
   function watcher(window) {
     try {
       callback(window);
@@ -182,166 +53,29 @@ function watchWindows(callback) {
     catch(ex) {}
   }
 
-  // Wait for the window to finish loading before running the callback
   function runOnLoad(window) {
-    // Listen for one load event before checking the window type
     window.addEventListener("load", function() {
       window.removeEventListener("load", arguments.callee, false);
-
-      // Now that the window has loaded, only handle browser windows
       let doc = window.document.documentElement;
       if (doc.getAttribute("windowtype") == "navigator:browser")
         watcher(window);
     }, false);
   }
 
-  // Add functionality to existing windows
+
   let browserWindows = Services.wm.getEnumerator("navigator:browser");
   while (browserWindows.hasMoreElements()) {
-    // Only run the watcher immediately if the browser is completely loaded
     let browserWindow = browserWindows.getNext();
-    if (browserWindow.document.readyState == "complete")
+    if (browserWindow.document.readyState == "complete") {
       watcher(browserWindow);
-    // Wait for the window to load before continuing
-    else
+    } else {
       runOnLoad(browserWindow);
-  }
-
-  // Watch for new browser windows opening then wait for it to load
-  function windowWatcher(subject, topic) {
-    if (topic == "domwindowopened")
-      runOnLoad(subject);
-  }
-  Services.ww.registerNotification(windowWatcher);
-
-  // Make sure to stop watching for windows if we're unloading
-  unload(function() Services.ww.unregisterNotification(windowWatcher));
-}
-
-/**
- * Save callbacks to run when unloading. Optionally scope the callback to a
- * container, e.g., window. Provide a way to run all the callbacks.
- *
- * @usage unload(): Run all callbacks and release them.
- *
- * @usage unload(callback): Add a callback to run on unload.
- * @param [function] callback: 0-parameter function to call on unload.
- * @return [function]: A 0-parameter function that undoes adding the callback.
- *
- * @usage unload(callback, container) Add a scoped callback to run on unload.
- * @param [function] callback: 0-parameter function to call on unload.
- * @param [node] container: Remove the callback when this container unloads.
- * @return [function]: A 0-parameter function that undoes adding the callback.
- */
-function unload(callback, container) {
-  // Initialize the array of unloaders on the first usage
-  let unloaders = unload.unloaders;
-  if (unloaders == null)
-    unloaders = unload.unloaders = [];
-
-  // Calling with no arguments runs all the unloader callbacks
-  if (callback == null) {
-    unloaders.slice().forEach(function(unloader) unloader());
-    unloaders.length = 0;
-    return;
-  }
-
-  // The callback is bound to the lifetime of the container if we have one
-  if (container != null) {
-    // Remove the unloader when the container unloads
-    container.addEventListener("unload", removeUnloader, false);
-
-    // Wrap the callback to additionally remove the unload listener
-    let origCallback = callback;
-    callback = function() {
-      container.removeEventListener("unload", removeUnloader, false);
-      origCallback();
     }
   }
-
-  // Wrap the callback in a function that ignores failures
-  function unloader() {
-    try {
-      callback();
-    }
-    catch(ex) {}
-  }
-  unloaders.push(unloader);
-
-  // Provide a way to remove the unloader
-  function removeUnloader() {
-    let index = unloaders.indexOf(unloader);
-    if (index != -1)
-      unloaders.splice(index, 1);
-  }
-  return removeUnloader;
 }
 
-/**
- * Shift the window's main browser content down and right a bit
- */
-function setupListener(window) {
-  function change(obj, prop, val) {
-    let orig = obj[prop];
-    obj[prop] = typeof val == "function" ? val(orig) : val;
-    unload(function() obj[prop] = orig, window);
-  }
-    
-  change(window.gBrowser, "loadOneTab", function(orig) {
-    return function(url) {
-      let tab = orig.apply(this, arguments);
-      if (url == "about:blank") {
-        let gBrowser = Services.wm.getMostRecentWindow("navigator:browser").gBrowser;
-        let fileURI = global.aboutURI.resolve('');
-        let tBrowser = gBrowser.getBrowserForTab(tab)
-        tBrowser.loadURI(fileURI, null, null);
-       
-        tab.linkedBrowser.addEventListener("load", function() {
-          tab.linkedBrowser.removeEventListener("load", arguments.callee, true);
-          Services.wm.getMostRecentWindow("navigator:browser").gURLBar.value = "";
-          let doc = tab.linkedBrowser.contentDocument;
-          let dashboard = new AwesomeTab(doc, global.utils, global.central, global.tagger);
-        }, true);
+function shutdown() {}
 
-      }
-      return tab;
-    };
-  });
-}
+function install() {}
 
-/**
- * Handle the add-on being activated on install/enable
- */
-function startup(data, reason) {
-  AddonManager.getAddonByID(data.id, function(addon) {
-    /* import scripts */
-    AWESOMETAB_SCRIPTS.forEach(function(fileName) {
-      let fileURI = addon.getResourceURI("scripts/" + fileName + ".js");
-      Services.scriptloader.loadSubScript(fileURI.spec, global);
-    });
-    global.aboutURI = addon.getResourceURI("content/awesometab.html");
-    global.central = new SiteCentral();
-    global.tagger = new POSTagger();
-    global.utils = new AwesomeTabUtils();
-    watchWindows(setupListener);
-  });
-}
-
-/**
- * Handle the add-on being deactivated on uninstall/disable
- */
-function shutdown(data, reason) {
-  // Clean up with unloaders when we're deactivating
-  if (reason != APP_SHUTDOWN)
-    unload();
-}
-
-/**
- * Handle the add-on being installed
- */
-function install(data, reason) {}
-
-/**
- * Handle the add-on being uninstalled
- */
-function uninstall(data, reason) {}
+function uninstall() {}
